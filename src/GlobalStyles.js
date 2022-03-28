05818449d8b6016f867ccf2886029ee31963b50a
Daniel Schrader <dhs@schrader-wolff-media.de>
2022-03-28 10:37:03 +0200

    Set up project with storybook, prettier and styled-components

---
+++ src/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 112.5%;
+    padding: 0;
+    margin: 0;
+  }
+
+  input, label, button, textarea {
+    font-size: 1em;
+  }
+`;
